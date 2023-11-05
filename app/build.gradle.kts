2367c37b22b9181282f511bbce51bfb5ea1599c5
günce <gncdmn.95@icloud.com>
2023-11-05 20:51:58 +0300

    Gradle problemi çözüldü

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("kotlin-kapt")
     id ("kotlin-parcelize")
     id ("androidx.navigation.safeargs.kotlin")
-    kotlin("kapt")
-    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -20,14 +19,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments += mapOf(
-                    "room.schemaLocation" to "$projectDir/schemas",
-                    "room.incremental" to "true"
-                )
-            }
-        }
     }
 
     buildTypes {
@@ -51,21 +42,12 @@ android {
         dataBinding = true
     }
 }
-kapt {
-    correctErrorTypes = true
-}
 
 dependencies {
-    val lifeCycleExtensionVersion = "1.1.1"
-    val supportVersion = "28.0.0"
     val retrofitVersion = "2.8.1"
     val glideVersion = "4.11.0"
-    val rxJavaVersion = "2.2.8"
-    val rxAndroidVersion = "2.1.1"
     val roomVersion = "2.6.0"
     val nav_version = "2.7.5"
-    val coroutineVersion = "1.3.9"
-    val hiltVersion = "2.44"
 
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -75,29 +57,25 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     //firebase
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-auth")
+
     //navigation
-    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
-    implementation("android.arch.lifecycle:extensions:$lifeCycleExtensionVersion")
+    //room
     implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
-    implementation("com.google.android.material:material:1.1.0")
+
+    //retrofit
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
-   // implementation("io.reactivex.rxjava2:rxjava:$rxJavaVersion")
-  //  implementation("io.reactivex.rxjava2:rxandroid:$rxAndroidVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    //glide
     implementation("com.github.bumptech.glide:glide:$glideVersion")
-    implementation ("com.android.support:design:$supportVersion")
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
-    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
 }
