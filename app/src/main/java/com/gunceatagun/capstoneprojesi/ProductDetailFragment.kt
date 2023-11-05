f4018256bcc29a7225712f9c0dc2fce88ac7000d
günce <gncdmn.95@icloud.com>
2023-11-05 22:39:16 +0300

    Recycler row item tasarlandı

--- app/src/main/java/com/gunceatagun/capstoneprojesi/ProductDetailFragment.kt
+++ app/src/main/java/com/gunceatagun/capstoneprojesi/ProductDetailFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.gunceatagun.capstoneprojesi.databinding.FragmentProductDetailBinding
 
 class ProductDetailFragment : Fragment() {
     private lateinit var binding: FragmentProductDetailBinding
+    private var productId = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,4 +20,16 @@ class ProductDetailFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        arguments?.let {
+//            productId = ProductDetailFragmentArgs.fromBundle(it).productId
+//            println(productId)
+//        }
+//        binding.listeyeGitButton.setOnClickListener {
+//            val action = ProductDetailFragmentDirections.actionProductDetailFragmentToProductsListFragment()
+//            Navigation.findNavController(it).navigate(action)
+//        }
+    }
 }
--- app/src/main/java/com/gunceatagun/capstoneprojesi/ProductsListFragment.kt
+++ app/src/main/java/com/gunceatagun/capstoneprojesi/ProductsListFragment.kt
@@ -1,19 +1,29 @@
 package com.gunceatagun.capstoneprojesi
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gunceatagun.capstoneprojesi.databinding.FragmentProductDetailBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.gunceatagun.capstoneprojesi.databinding.FragmentProductsListBinding
 
 class ProductsListFragment : Fragment() {
-    private lateinit var binding: FragmentProductDetailBinding
+    private lateinit var binding: FragmentProductsListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentProductDetailBinding.inflate(inflater, container, false)
+        binding = FragmentProductsListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.detayaGitButton.setOnClickListener {
+//            val action = ProductsListFragmentDirections.actionProductsListFragmentToProductDetailFragment(3)
+//            Navigation.findNavController(it).navigate(action)
+//        }
     }
 }
