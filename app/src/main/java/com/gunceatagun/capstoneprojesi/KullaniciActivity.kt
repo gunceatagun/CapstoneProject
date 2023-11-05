aff37b70ce60c971330371194efcc31af33eeb9d
günce <gncdmn.95@icloud.com>
2023-11-05 15:47:42 +0300

    Giriş yap Kayıt ol fonksiyon eklenmesi

---
+++ app/src/main/java/com/gunceatagun/capstoneprojesi/KullaniciActivity.kt
@@ -0,0 +1,63 @@
+package com.gunceatagun.capstoneprojesi
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.gunceatagun.capstoneprojesi.databinding.ActivityAnasayfaBinding
+import com.gunceatagun.capstoneprojesi.databinding.ActivityKullaniciBinding
+
+class KullaniciActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityKullaniciBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityKullaniciBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        //kullanıcı giriş yapmışsa tekrar giriş yapmayacak.
+        val guncelKullanici = auth.currentUser
+        if(guncelKullanici != null ){
+            val intent = Intent(this, AnasayfaActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun girisYap(view: View) {
+        auth.signInWithEmailAndPassword(
+            binding.emailText.text.toString(),
+            binding.passwordText.text.toString()
+        ).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val guncelKullanici = auth.currentUser?.email.toString()
+                Toast.makeText(this,"Hoşgeldin: ${guncelKullanici}",Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this, AnasayfaActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun kayitOl(view: View) {
+        val email = binding.emailText.text.toString()
+        val sifre = binding.passwordText.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, sifre).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, AnasayfaActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+}
