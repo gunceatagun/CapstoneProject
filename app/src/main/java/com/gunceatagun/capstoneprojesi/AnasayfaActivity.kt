96cb090ea692cf728c897002ee45a8d7eb8aba36
günce <gncdmn.95@icloud.com>
2023-11-05 16:26:49 +0300

    Çıkış yap menüye eklendi fonksiyonu eklendi

--- app/src/main/java/com/gunceatagun/capstoneprojesi/AnasayfaActivity.kt
+++ app/src/main/java/com/gunceatagun/capstoneprojesi/AnasayfaActivity.kt
@@ -1,11 +1,33 @@
 package com.gunceatagun.capstoneprojesi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class AnasayfaActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anasayfa)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.cikis_yap_menusu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.cikis_yap) {
+            auth.signOut()
+            val intent = Intent(this, KullaniciActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
